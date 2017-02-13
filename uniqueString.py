06cf52f9cfd09e5000e791f89d1c4416efeae1af
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-02-13 02:03:51 -0800

    added comments

--- uniqueString.py
+++ uniqueString.py
@@ -3,12 +3,19 @@ def isUnique(str):
     checker  = 0
     strList = list(str)
     for i in range(len(strList)):
+
+        # ord is used to find ASCII value of character in python
         val = ord(strList[i]) - ord('a')
+
+        # if ASCII value of their differences was already added by shifting 1 as many times in bitwise
+        # representation of checker, then this time bitwise & will return true as at least one bit will be
+        # set high in bitwise and
         if( checker & (1 << val) > 0):
             return False
-        #checker |= (1 << val)
+
+        #left shift 1 and add it in bitwise representation of checker
         checker = checker | (1 << val)
-        #a += 7
+        # checker |= (1 << val)
 
     return True
 
