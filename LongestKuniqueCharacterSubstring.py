22cc1024c4a0cc9f9254e90660e04c5b759f05d2
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-02-27 18:32:51 -0800

    Added edge cases in Longest K unique character string

--- LongestKuniqueCharacterSubstring.py
+++ LongestKuniqueCharacterSubstring.py
@@ -1,10 +1,16 @@
 def LongestKUniqueCharactersSubstring(inputstr, K):
 
-    start,end = 0,0
+    if(len(inputstr) < K):
+        return False
 
+    # start and end variable for moving index in string
+    start,end = 0,0
+    # initially length is at least 1, considering string is not blank
     windowsSize, windowStart = 1,0
 
+    #  keeping dictionary with character and counts of their occurence
     dict = {}
+    # first character with value 0
     dict[inputstr[0:1]] = 1
 
     for i in range(1,len(inputstr)):
@@ -22,18 +28,26 @@ def LongestKUniqueCharactersSubstring(inputstr, K):
             windowsSize = end - start + 1
             windowStart = start
 
-
-    return inputstr[windowStart:windowStart+ windowsSize]
-
+        if IsResultvalid(inputstr[windowStart:windowStart+ windowsSize], dict, K):
+            return inputstr[windowStart:windowStart+ windowsSize]
+        else:
+            return False
 
 def IsLessThan(dict, K):
     count = 0
-
     for key in dict:
         if dict[key] > 0:
             count += 1
 
     return (count <= K)
+
+def IsResultvalid(str, dict, K):
+    count = 0
+    for key in dict:
+        if dict[key] > 0:
+            count += 1
+
+    return (count == K)
 
 
 answer = LongestKUniqueCharactersSubstring("abbbbccdddddddfeeeeeeeee", 2)
@@ -41,4 +55,6 @@ print(answer)
 answer = LongestKUniqueCharactersSubstring("karappa", 2)
 print(answer)
 answer = LongestKUniqueCharactersSubstring("abbbbccddddddddddfeeeeeeeee", 2)
+print(answer)
+answer = LongestKUniqueCharactersSubstring("abbbbcccccccc", 4)
 print(answer)
