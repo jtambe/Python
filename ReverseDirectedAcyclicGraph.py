0f3c039e91df5fe3ac7355a8b8a943a57d08678d
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-02-27 19:07:31 -0800

    How to reverse a given Directed Acyclic Graph

---
+++ ReverseDirectedAcyclicGraph.py
@@ -0,0 +1,40 @@
+
+
+
+# How to reverse a given Directed Acyclic Graph
+
+def ReverseDAG(graph):
+
+    newGraph = {}
+
+
+    for k in graph:
+        # print(graph[k])
+        for item in (graph[k]):
+            # print(item, end=",")
+            if item in newGraph:
+                newGraph[item].append(k)
+            else:
+                newGraph[item] = []
+                newGraph[item].append(k)
+
+
+    return newGraph
+
+
+
+
+
+
+def main():
+    graph = {0:[1,2], 1:[2], 2:[3], 3:[4], 4:[5], 5:[6], 6:[3,7]}
+    print(graph)
+    reverse = ReverseDAG(graph)
+    print(reverse)
+
+    # for node in (graph["2"]):
+    #     print(node)
+
+
+if __name__ == "__main__":
+    main()
